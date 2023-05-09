5cfdc8bdba1439b2e55000837d2b33b58915cae0
janielcarneiro <janielcl16@gmail.com>
2023-05-09 11:24:54 -0300

    BD Veiculos e Alocações e Users configurados os arquivos deles

---
+++ model/Alocacoes.js
@@ -0,0 +1,28 @@
+/*const {DataTypes} = require('sequelize');
+
+const db = require('../db/conn');
+
+const Veiculos = db.define('Veiculos', {
+    modelo: {
+        type: DataTypes.STRING,
+        required: true
+    },
+
+    placa: {
+        type: DataTypes.NUMBER,
+        required: true
+    },
+
+    custo: {
+        type: DataTypes.STRING,
+        required: true
+    },
+
+    cor: {
+        type: DataTypes.STRING,
+        required: true
+    }
+    
+});
+
+module.exports = Veiculos;*/
