d94fdf964eb65b02a936ec78551cb19619195b62
jordanferreiraa <jordanferreira69@gmail.com>
2023-05-10 22:30:55 -0300

    model Client

---
+++ model/Client.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require('sequelize')
+const db = require('../db/conn')
+
+
+const Client = db.define('Client', {
+  nome: {
+    type: DataTypes.STRING,
+    required: true
+  },
+  cpf: {
+    type: DataTypes.INTEGER,
+    required: true
+  },
+  telefone: {
+    type: DataTypes.INTEGER,
+    required: true
+  },
+  endereco: {
+    type: DataTypes.STRING,
+    required: true
+  }
+})
+
+module.exports = Client
