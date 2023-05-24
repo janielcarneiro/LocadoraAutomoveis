cf397ddb0c8716fdef1e41c117c80efd84a6d5af
jordanferreiraa <jordanferreira69@gmail.com>
2023-05-24 15:36:50 -0300

    update user

--- controllers/users.js
+++ controllers/users.js
@@ -38,5 +38,57 @@ module.exports = class users {
         res.render('home', { user });
       
     }
+
+    static newUser(req, res) {
+      res.render('dashboard/user/a_user')
+    }
+  
+    static async newUserSave(req, res) {
+      
+      const usuario = {
+        nome: req.body.nome,
+        cpf: req.body.cpf,
+        login: req.body.login,
+        senha: req.body.senha,
+      }
+      await User.create(usuario)
+      
+      res.redirect('/users')
+    }
+  
+    static async allUsers(req, res) {
+      const users = await User.findAll({ raw: true })
+      res.render('dashboard/user/user', { users })
+    }
+  
+    static async updateUser(req, res) {
+      const id = req.params.id
+      const user = await User.findOne({ where:{id:id}, raw:true })
+      res.render('dashboard/user/e_user', { user })
+    }
+  
+    static async updateUserSave(req, res) {
+      const id = req.body.id
+      const user = {
+        nome: req.body.nome,
+        cpf: req.body.cpf,
+        login: req.body.login,
+        senha: req.body.senha,
+      }
+      await User.update(user, { where: {id: id} })
+      .then(res.redirect('/users'))
+      .catch((err) => {
+        console.log(err)
+      })
+    }
+  
+    static async removeUser(req, res) {
+      const id = req.body.id
+      await User.destroy({ where: {id: id} })
+      .then(res.redirect('/users'))
+      .catch((err) => {
+        console.log(err)
+      })
+    }
   }
   
--- routes/users.js
+++ routes/users.js
@@ -12,4 +12,15 @@ router.post('/logout', users.logout)
 router.get('/dashboard', users.dashboard)
 router.get('/', users.home)
 
+
+router.get('/users/add', users.newUser)
+router.post('/users/add', users.newUserSave)
+
+router.get('/users/edit/:id', users.updateUser)
+router.post('/users/edit', users.updateUserSave)
+
+router.post('/users/remove', users.removeUser)
+
+router.get('/users', users.allUsers)
+
 module.exports = router
