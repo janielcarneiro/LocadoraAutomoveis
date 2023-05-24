2734f9a0092e78db22d5ecd774d589f9b74c1485
janielcarneiro <janielcl16@gmail.com>
2023-05-24 11:39:22 -0300

    Já fiz a parte de logout

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,7 @@ const router = express.Router();
 
 router.get('/login',B_login, users.login)
 router.post('/loginpost', users.login_post)
+router.post('/logout', users.logout)
 
 router.get('/dashboard',V_session, users.dashboard)
 router.get('/', users.home)
